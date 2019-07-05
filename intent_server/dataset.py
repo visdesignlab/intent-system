9651fb1a13e5a2bcd693697d52b1e355784978a0
Jochen Görtler <grtlr@users.noreply.github.com>
2019-07-05 13:37:10 -0600

    Load all datasets and provide API (#6)

--- intent_server/dataset.py
+++ intent_server/dataset.py
@@ -6,12 +6,13 @@ from typing import Dict
 
 
 class Dataset:
-    def __init__(self, label: str, data: pd.DataFrame) -> None:
+    def __init__(self, label: str, data: pd.DataFrame, name: str) -> None:
         self.label: str = label
         self.data: pd.DataFrame = data
+        self.name: str = name
 
     def to_dict(self) -> Dict:
-        output = {'labelColumn': self.label}
+        output = {'labelColumn': self.label, 'name': self.name}
         output['values'] = self.data.T.to_dict()
         return output
 
@@ -19,9 +20,59 @@ class Dataset:
         return self.data.select_dtypes(include='number')
 
     @staticmethod
-    def from_json_file(filename: str) -> 'Dataset':
+    def from_json_file(filename: str, name: str) -> 'Dataset':
         with open(filename) as read_file:
             raw_json = json.load(read_file)
             label = raw_json['labelColumn']
             data = pd.DataFrame.from_dict(raw_json['values'], orient='index')
-            return Dataset(label, data)
+            return Dataset(label, data, name)
+
+    @staticmethod
+    def load_housing_data() -> 'Dataset':
+        df = pd.concat([
+            pd.read_csv('data/housing/train1.csv', thousands=','),
+            pd.read_csv('data/housing/train2.csv', thousands=','),
+        ])
+        selection = df[[
+            'ListNo',
+            'Acres',
+            'BsmntFin',
+            'Deck',
+            'EWCoord',
+            'GaragCap',
+            'HouseNbr',
+            'LstPrice',
+            'NSCoord',
+            'Taxes',
+            'TotBed',
+            'TotBth',
+            'TotSqf',
+            'YearBlt',
+        ]]
+        if selection.isna().values.any():
+            selection = selection.fillna(0)
+        return Dataset('ListNo', selection.astype({'ListNo': str}), 'SLC Housing')
+
+    @staticmethod
+    def load_nba_data() -> 'Dataset':
+        df = pd.read_csv('data/nba_all_seasons.csv')
+        is_season = df['season'] == '2016-17'
+        return Dataset('player_name', df[is_season].drop(columns=[
+            'Unnamed: 0',
+            'season',
+            'college',
+            'draft_year',
+            'draft_round',
+            'draft_number',
+        ]), 'NBA Players')
+
+    @staticmethod
+    def load_draft_combine_data() -> 'Dataset':
+        df = pd.read_csv('data/2018_draft_combine.csv')
+        return Dataset('Name', df.drop(columns=[
+            'Team',
+            'Position Group',
+            'Position',
+            'Combine ID',
+            'URL',
+        ]), 'NFL Draft Combine')
