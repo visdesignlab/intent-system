65a414f92ae609681613c2fac27031e5ece53323
Jochen Görtler <web@goertler.email>
2019-07-05 13:31:15 -0600

    Load all datasets and provide API

--- intent_server/views.py
+++ intent_server/views.py
@@ -4,12 +4,18 @@ from .dataset import Dataset
 
 
 # Load and preprocess the dataset
-ds = Dataset.from_json_file('data/slc_housing.json')
+datasets = {
+ 'slc_housing': Dataset.load_housing_data(),
+ 'draft_combine': Dataset.load_draft_combine_data(),
+ 'nba_players': Dataset.load_nba_data(),
+}
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/dataset')
-def route_dataset():  # type: ignore
-    print(jsonify(ds.to_dict()))
-    return jsonify(ds.to_dict())
+def route_dataset_list():  # type: ignore
+    return jsonify(list(datasets.keys()))
+
+@views.route('/dataset/<dataset_name>')
+def route_dataset(dataset_name):  # type: ignore
+    return jsonify(datasets[dataset_name].to_dict())
