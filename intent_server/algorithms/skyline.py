e6ad6eb9598de384cac22a384b573ef0fbd9caec
Jochen Görtler <web@goertler.email>
2019-07-15 12:14:27 -0600

    WIP:

---
+++ intent_server/algorithms/skyline.py
@@ -0,0 +1,23 @@
+from ..properties import Measure
+
+import pandas as pd
+
+
+
+class Skyline(Measure):
+    def __init__(self) -> None:
+
+    def maxima1(S,d): -> List[Index]:
+        M: List[Index]
+        M_star: List[Index]
+
+
+
+    def compute(self, df: pd.DataFrame) -> pd.DataFrame:
+        # The following implementation comes from
+        # `Computational Geometry: an introduction` by Preparata and Shamos.
+        
+        
+
+        return pd.DataFrame(data=[42], columns=['Skyline']) 
+
