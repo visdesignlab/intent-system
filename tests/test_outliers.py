c5763169fc12715fabe43d77b10475c0929a90bb
Jochen Görtler <grtlr@users.noreply.github.com>
2019-07-17 14:55:59 -0600

    Actually run the test cases on Travis 🤯 (#21)

--- tests/test_outliers.py
+++ tests/test_outliers.py
@@ -1,10 +1,11 @@
 from intent_server.properties import Outlier
 
 import pandas as pd
+import numpy as np
 
 
 def test_measure_outlier():
     d = {'col1': [1, 2, 1, 2, 1000], 'col2': [2, 1, 1, 2, 1000]}
     df = pd.DataFrame(data=d)
-    outls = Outlier(2, 0.1)
-    assert (outls.compute(df) == [1, 1, 1, 1, -1]).all()
+    outls = Outlier(2, 0.1).compute(df).values.transpose()
+    assert np.array_equal(outls, np.array([1, 1, 1, 1, -1]).reshape([1, 5]))
