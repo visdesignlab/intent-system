0ebeac1754e8af44ac728de7130c9af55f54279a
Jochen Görtler <web@goertler.email>
2019-07-17 14:46:08 -0600

    Format

--- tests/test_outliers.py
+++ tests/test_outliers.py
@@ -8,4 +8,4 @@ def test_measure_outlier():
     d = {'col1': [1, 2, 1, 2, 1000], 'col2': [2, 1, 1, 2, 1000]}
     df = pd.DataFrame(data=d)
     outls = Outlier(2, 0.1).compute(df).values.transpose()
-    assert np.array_equal(outls, np.array([1, 1, 1, 1, -1]).reshape([1,5]))
+    assert np.array_equal(outls, np.array([1, 1, 1, 1, -1]).reshape([1, 5]))
