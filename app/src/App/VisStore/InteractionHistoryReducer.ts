08944c4135be57a6e8a4758b812b0f62b51911b5
Kiran Gadhave <kirangadhave2@gmail.com>
2019-07-23 16:33:36 -0600

    Reformatted to handle NaN values

--- app/src/App/VisStore/InteractionHistoryReducer.ts
+++ app/src/App/VisStore/InteractionHistoryReducer.ts
@@ -1,5 +1,7 @@
 import { Action, Reducer } from "redux";
 import { Interaction, InteractionHistory } from "@visdesignlab/intent-contract";
+import axios from "axios";
+import { datasetName } from "../..";
 
 export enum InteractionHistoryActions {
   ADD_INTERACTION = "ADD_INTERACTION"
@@ -18,6 +20,9 @@ export const InteractionHistoryReducer: Reducer<
   switch (action.type) {
     case InteractionHistoryActions.ADD_INTERACTION:
       current = [...current, action.args];
+      axios.post(`/dataset/${datasetName}/predict`, current).then(response => {
+        console.log("Predictions", response.data);
+      });
       return [...current];
     default:
       return [...current];
