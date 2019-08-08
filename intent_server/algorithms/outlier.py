c41c4f1c4357bd97242a75c2e1ff84c71897fdb9
Jochen Görtler <web@goertler.email>
2019-08-08 10:45:43 -0600

    Use default values

--- intent_server/algorithms/outlier.py
+++ intent_server/algorithms/outlier.py
@@ -4,11 +4,11 @@ from ..intent import IntentBinary
 
 import pandas as pd
 
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Union
 
 
 class Outlier(IntentBinary):
-    def __init__(self, n_neighbors: int, contamination: float) -> None:
+    def __init__(self, n_neighbors: int = 20, contamination: Union[float,str] = 'auto') -> None:
         self.clf = LocalOutlierFactor(n_neighbors=n_neighbors, contamination=contamination)
 
     def to_string(self) -> str:
