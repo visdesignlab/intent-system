1b97c84ab0012848c8ed6866af5ae31cefcbda41
Kiran Gadhave <14944083+kirangadhave@users.noreply.github.com>
2019-08-08 13:00:21 -0600

    Centered vis

--- app/src/Components/Visualization/ScatterPlotMatrix/ScatterPlotMatrix.tsx
+++ app/src/Components/Visualization/ScatterPlotMatrix/ScatterPlotMatrix.tsx
@@ -4,6 +4,8 @@ import DimensionSelector from "../../DimensionSelector/DimensionSelector";
 import SPMComponent from "./SPMComponent";
 import FullSizeSVG from "../ReusableComponents/FullSizeSVG";
 import { VisualizationType } from "@visdesignlab/intent-contract";
+import styles from "./scatterplotmatrix.module.css";
+import { select } from "d3";
 
 interface State {
   svgHeight: number;
@@ -24,6 +26,7 @@ type Props = OwnProps & DispatchProps & StateProps;
 
 class ScatterPlotMatrix extends React.Component<Props, State> {
   ref: React.RefObject<SVGSVGElement> = React.createRef();
+  divRef: React.RefObject<HTMLDivElement> = React.createRef();
 
   readonly state: State = {
     svgHeight: 0,
@@ -34,9 +37,17 @@ class ScatterPlotMatrix extends React.Component<Props, State> {
   componentDidMount() {
     const { dimensions } = this.props;
 
+    const divHeight = (this.divRef.current as HTMLDivElement).clientHeight;
+    const divWidth = (this.divRef.current as HTMLDivElement).clientWidth;
+    const lesser = divHeight < divWidth ? divHeight : divWidth;
+
+    select(this.divRef.current)
+      .style("height", `${lesser}px`)
+      .style("width", `${lesser}px`);
+
     this.setState({
-      svgHeight: (this.ref.current as SVGSVGElement).clientHeight,
-      svgWidth: (this.ref.current as SVGSVGElement).clientWidth,
+      svgHeight: lesser,
+      svgWidth: lesser,
       selectedDimensions: dimensions.slice(0, 3)
     });
     // this.props.addEmptyInteraction(VisualizationType.ScatterPlot, [
@@ -54,9 +65,17 @@ class ScatterPlotMatrix extends React.Component<Props, State> {
   }
 
   resize() {
+    const divHeight = (this.divRef.current as HTMLDivElement).clientHeight;
+    const divWidth = (this.divRef.current as HTMLDivElement).clientWidth;
+    const lesser = divHeight < divWidth ? divHeight : divWidth;
+
+    select(this.divRef.current)
+      .style("height", `${lesser}px`)
+      .style("width", `${lesser}px`);
+
     this.setState({
-      svgHeight: (this.ref.current as SVGSVGElement).clientHeight,
-      svgWidth: (this.ref.current as SVGSVGElement).clientWidth
+      svgHeight: lesser,
+      svgWidth: lesser
     });
   }
 
@@ -90,22 +109,28 @@ class ScatterPlotMatrix extends React.Component<Props, State> {
             });
           }}
         />
-        <FullSizeSVG ref={this.ref}>
-          <g transform={`translate(100,100)`}>
-            <SPMComponent
-              vis={VisualizationType.ScatterPlotMatrix}
-              data={data}
-              height={lesserDim - 100}
-              width={lesserDim - 100}
-              labels={labels}
-              columns={selectedDimensions}
-              XZero={false}
-              YZero={false}
-              XOffset={lesserDim * 0.05}
-              YOffset={lesserDim * 0.06}
-            />
-          </g>
-        </FullSizeSVG>
+        <div
+          ref={this.divRef}
+          className={styles.square}
+          style={{ border: "1px solid red" }}
+        >
+          <FullSizeSVG ref={this.ref}>
+            <g transform={`translate(50,50)`}>
+              <SPMComponent
+                vis={VisualizationType.ScatterPlotMatrix}
+                data={data}
+                height={lesserDim - 100}
+                width={lesserDim - 100}
+                labels={labels}
+                columns={selectedDimensions}
+                XZero={false}
+                YZero={false}
+                XOffset={lesserDim * 0.05}
+                YOffset={lesserDim * 0.06}
+              />
+            </g>
+          </FullSizeSVG>
+        </div>
       </ScatterPlotDiv>
     );
   }
