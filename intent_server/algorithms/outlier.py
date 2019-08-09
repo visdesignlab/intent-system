3f69fee1cd90bbdd1b3da23e7e4347ff43224b09
Jochen Görtler <web@goertler.email>
2019-08-09 14:44:27 -0600

    Formatting

--- intent_server/algorithms/outlier.py
+++ intent_server/algorithms/outlier.py
@@ -8,7 +8,7 @@ from typing import Optional, Dict, Any, Union
 
 
 class Outlier(IntentBinary):
-    def __init__(self, n_neighbors: int = 20, contamination: Union[float,str] = 'auto') -> None:
+    def __init__(self, n_neighbors: int = 20, contamination: Union[float, str] = 'auto') -> None:
         self.clf = LocalOutlierFactor(n_neighbors=n_neighbors, contamination=contamination)
 
     def to_string(self) -> str:
@@ -17,8 +17,9 @@ class Outlier(IntentBinary):
     def compute(self, df: pd.DataFrame) -> pd.DataFrame:
         nan_dropped = df.dropna()
         outliers = self.clf.fit_predict(nan_dropped)
-        result = pd.DataFrame(data=outliers, index=nan_dropped.index, columns=[self.to_string()]).replace({-1:1, 1:0});
-        return result.loc[result.ix[:,0] == 1].reindex(index=df.index, fill_value=0)
+        result = pd.DataFrame(data=outliers, index=nan_dropped.index, columns=[
+                              self.to_string()]).replace({-1: 1, 1: 0})
+        return result.loc[result.ix[:, 0] == 1].reindex(index=df.index, fill_value=0)
 
     def info(self) -> Optional[Dict[str, Any]]:
         return {
