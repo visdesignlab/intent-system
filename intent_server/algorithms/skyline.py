63fc6483aff7714d5fe74fd081c7d68d14c4b979
Jochen Görtler <web@goertler.email>
2019-08-26 10:36:15 -0600

    Implement all skyline permutations

--- intent_server/algorithms/skyline.py
+++ intent_server/algorithms/skyline.py
@@ -1,7 +1,8 @@
-from ..intent import IntentBinary
+from ..intent import IntentMulticlass
 
 import pandas as pd
 import numpy as np
+import itertools
 
 from typing import Optional, Dict, Any
 
@@ -21,7 +22,11 @@ def belongs_to_skyline(data: np.ndarray, pt: np.ndarray) -> bool:
     return np.apply_along_axis(lambda x: not(dominates(x, pt)), 1, data).all()  # type: ignore
 
 
-class Skyline(IntentBinary):
+def permutation_to_str(data: np.ndarray) -> str:
+    return ';'.join(map(lambda x: 'Max' if x == -1 else 'Min', data))
+
+
+class Skyline(IntentMulticlass):
     def __init__(self) -> None:
         pass
 
@@ -29,12 +34,26 @@ class Skyline(IntentBinary):
         return 'Skyline'
 
     def compute(self, df: pd.DataFrame) -> pd.DataFrame:
-        data = df.select_dtypes(include=['number']).values
-        skyline = np.apply_along_axis(lambda x: belongs_to_skyline(data, x), 1, data)
-        return pd.DataFrame(
-            index=df.index,
-            data=skyline,
-            columns=[self.to_string()]).applymap(lambda x: 1 if x else 0)
+        data = df.select_dtypes(include=['number'])
+
+        # Get all permutations of the sign
+        permutations = [list(i) for i in itertools.product([-1, 1], repeat=len(data.columns))]
+
+        result = pd.concat(map(lambda perm: self.skyline_for_permutation(
+            np.array(perm), data), permutations), axis='columns')
+        return result
 
     def info(self) -> Optional[Dict[str, Any]]:
         return None
+
+    def skyline_for_permutation(self, signs: np.ndarray, data: pd.DataFrame) -> pd.DataFrame:
+        vs = data.values.dot(np.diag(signs))
+        skyline = np.apply_along_axis(lambda x: belongs_to_skyline(vs, x), 1, vs)
+        return pd.DataFrame(
+            index=data.index,
+            data=skyline,
+            columns=[
+                self.to_string() +
+                ":" +
+                permutation_to_str(signs)]).applymap(
+            lambda x: 1 if x else 0)
