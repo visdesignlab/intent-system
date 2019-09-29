7cf80b0b965098902dd1a5efa1248b69eb127593
Kiran Gadhave <kirangadhave2@gmail.com>
2019-09-29 13:50:06 -0600

    Adding new scatterplot visualization

---
+++ app/src/Components/Visualization/ScatterGrid/ScatterplotGrid.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+interface OwnProps {}
+
+type Props = OwnProps;
+const ScatterplotGrid: FC<Props> = ({  }: Props) => {
+  return <g></g>;
+};
+
+export default ScatterplotGrid;
