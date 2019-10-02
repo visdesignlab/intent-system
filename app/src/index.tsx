7d1a3848343caab0a6c0f3b2678872c967ec9d79
Kiran Gadhave <kirangadhave2@gmail.com>
2019-10-02 11:54:26 -0600

    Adding dataset

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,40 +1,12 @@
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
-
+import axios from 'axios';
 import {initProvenanceRedux} from '@visdesignlab/provenance-lib-core/lib/src';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 
-import App from './App/App';
-import {initVisStoreWithProvenance} from './App/ProvenanceVisStore/VisStoreProvenance';
-import {loadDataset} from './App/VisStore/DatasetReducers';
 import * as serviceWorker from './serviceWorker';
-import {initStudyStore, StudyState} from './StudyStore/StudyStore';
-
-// import { VisualizationType } from "@visdesignlab/intent-contract";
-function initAppStore() {
-  let store = initStudyStore();
-  return {
-    store: () => store,
-    resetStore: (state?: StudyState) =>
-      (store = state ? initStudyStore(state) : initStudyStore()),
-  };
-}
-
-const studyStore = initAppStore();
-
-export const StudyProvenance = initProvenanceRedux(
-  studyStore.store(),
-  studyStore.resetStore,
-);
-
-export const VisStore = initVisStoreWithProvenance();
-
-export const VisProvenance = initProvenanceRedux(
-  VisStore.visStore(),
-  VisStore.resetStore,
-);
 
 export const datasets = [
   'draft_combine',
@@ -43,32 +15,21 @@ export const datasets = [
   'gapminder_world',
 ];
 
-// export const datasetName = "draft_combine";
 export let datasetName = 'gapminder_world';
-// export const datasetName = "nba_players";
 
-VisStore.visStore().dispatch(loadDataset(`/dataset/${datasetName}`) as any);
+axios
+  .get('/dataset')
+  .then(response => {
+    const datasets = response.data;
 
-export function changeDataset(dsName: string) {
-  if (datasetName === dsName) return;
-  datasetName = dsName;
-  VisStore.resetStore();
-  VisStore.visStore().dispatch(loadDataset(`/dataset/${datasetName}`) as any);
-  renderApp();
-}
+    const defaultDataset = datasets.filter((d: string) =>
+      d.includes('gapminder'),
+    )[0];
+    console.log(defaultDataset);
+  })
+  .catch(err => console.log(err));
 
-function renderApp() {
-  ReactDOM.render(
-    <Provider store={studyStore.store()}>
-      <Provider store={VisStore.visStore()}>
-        <App />
-      </Provider>
-    </Provider>,
-    document.getElementById('root'),
-  );
-}
-
-renderApp();
+ReactDOM.render(<div>Hello, World!</div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
