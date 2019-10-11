3ba6c50b94ed830a11829242c592b9a9fd7e844f
Kiran Gadhave <kirangadhave2@gmail.com>
2019-10-11 17:45:52 -0600

    Setup Study state

--- app/src/Stores/Study/StudyState.ts
+++ app/src/Stores/Study/StudyState.ts
@@ -1,1 +1,12 @@
-export default interface Study {}
+import ParticipantDetails from '../Types/ParticipantDetails';
+import TaskDetails from '../Types/TaskDetails';
+import Events from '../Types/EventEnum';
+
+export default interface StudyState {
+  participant: ParticipantDetails;
+  task: TaskDetails;
+  event: Events;
+  startTime: Date;
+  endTime: Date;
+  eventTime: Date;
+}
