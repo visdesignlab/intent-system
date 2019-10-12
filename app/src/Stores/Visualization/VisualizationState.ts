d34fc5cb263772142f474c9085df8586067e8441
Kiran Gadhave <kirangadhave2@gmail.com>
2019-10-12 16:30:56 -0600

    Added basic dataset loader function

--- app/src/Stores/Visualization/VisualizationState.ts
+++ app/src/Stores/Visualization/VisualizationState.ts
@@ -1,14 +1,14 @@
 import {Interaction} from '@visdesignlab/intent-contract';
-import {Dataset} from './Dataset';
 import {Plots} from '../Types/Plots';
 import TaskDetails from '../Types/TaskDetails';
 import ParticipantDetails from '../Types/ParticipantDetails';
+import {Dataset} from '../Types/Dataset';
 
 export default interface VisualizationState {
-  participant: ParticipantDetails;
-  task: TaskDetails;
+  participant?: ParticipantDetails;
+  task?: TaskDetails;
   dataset: Dataset;
-  plots: Plots;
-  interaction: Interaction;
-  multiBrushBehaviour: boolean;
+  plots?: Plots;
+  interaction?: Interaction;
+  multiBrushBehaviour?: boolean;
 }
