8e6411b2d2d052b233ea85a30ee4ba82f1257c39
Kiran Gadhave <kirangadhave2@gmail.com>
2019-10-16 07:21:33 -0600

    Added multi brush highlighting

--- app/src/Components/Scatterplot.tsx
+++ app/src/Components/Scatterplot.tsx
@@ -1,13 +1,22 @@
-import { axisBottom, axisLeft, max, min, scaleLinear, scaleOrdinal, schemeSet2, select } from 'd3';
-import React, { createRef, FC, RefObject, useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { Popup } from 'semantic-ui-react';
+import {
+  axisBottom,
+  axisLeft,
+  max,
+  min,
+  scaleLinear,
+  scaleOrdinal,
+  schemeSet2,
+  select,
+} from 'd3';
+import React, {createRef, FC, RefObject, useEffect, useState} from 'react';
+import {connect} from 'react-redux';
+import {Popup} from 'semantic-ui-react';
 import styled from 'styled-components';
 
-import { VisualizationProvenance } from '..';
-import { Dataset } from '../Stores/Types/Dataset';
-import { SinglePlot } from '../Stores/Types/Plots';
-import { removePlot } from '../Stores/Visualization/Setup/PlotsRedux';
+import {VisualizationProvenance} from '..';
+import {Dataset} from '../Stores/Types/Dataset';
+import {SinglePlot} from '../Stores/Types/Plots';
+import {removePlot, updatePlot} from '../Stores/Visualization/Setup/PlotsRedux';
 import VisualizationState from '../Stores/Visualization/VisualizationState';
 import BrushComponent from './Brush/Components/BrushComponent';
 
@@ -22,6 +31,7 @@ interface StateProps {
 }
 
 interface DispatchProps {
+  updatePlot: (plot: SinglePlot) => void;
   removePlot: (plot: SinglePlot) => void;
 }
 
@@ -32,16 +42,17 @@ const Scatterplot: FC<Props> = ({
   size,
   dataset,
   lastPlot,
-  removePlot
+  updatePlot,
+  removePlot,
 }: Props) => {
   const xAxisRef: RefObject<SVGGElement> = createRef();
   const yAxisRef: RefObject<SVGGElement> = createRef();
 
-  const { x, y, color, id, brushes } = plot;
+  const {x, y, color, brushes} = plot;
   const data = dataset.data.map(v => ({
     x: v[x],
     y: v[y],
-    color: v[color]
+    color: v[color],
   }));
 
   const [mouseIsDown, setMouseIsDown] = useState(false);
@@ -71,21 +82,54 @@ const Scatterplot: FC<Props> = ({
     }
   }, [size, xAxisRef, xScale, yAxisRef, yScale]);
 
+  const selectedIndices: {[key: number]: number} = {};
+
+  const extentPadding = 5;
+  const [height, width] = [
+    Math.abs(paddedSize + extentPadding - (0 - extentPadding)),
+    Math.abs(0 - extentPadding - (paddedSize + extentPadding)),
+  ];
+
+  let maxIntersection = 1;
+
+  Object.values(brushes).forEach(brush => {
+    let {x1, x2, y1, y2} = brush.extents;
+    [x1, x2, y1, y2] = [
+      xScale.invert(x1 * width - 5),
+      xScale.invert(x2 * width - 5),
+      yScale.invert(y1 * height - 5),
+      yScale.invert(y2 * height - 5),
+    ];
+
+    data.forEach((d, i) => {
+      if (d.x >= x1 && d.x <= x2 && d.y <= y1 && d.y >= y2) {
+        if (!selectedIndices[i]) {
+          selectedIndices[i] = 0;
+        }
+        selectedIndices[i] += 1;
+        if (selectedIndices[i] > maxIntersection)
+          maxIntersection = selectedIndices[i];
+      }
+    });
+  });
+
   const BrushLayer = (
     <g
       onMouseDown={() => setMouseIsDown(true)}
       onMouseUp={() => setMouseIsDown(false)}
-      onMouseOut={() => setMouseIsDown(false)}
-    >
+      onMouseOut={() => setMouseIsDown(false)}>
       <BrushComponent
         extents={{
           left: 0,
           top: 0,
           right: paddedSize,
-          bottom: paddedSize
+          bottom: paddedSize,
         }}
-        extentPadding={5}
+        extentPadding={extentPadding}
         onBrushUpdate={(brushes, affectedBrush, affectType) => {
+          const currPlot = {...plot};
+          currPlot.brushes = {...brushes};
+          updatePlot(currPlot);
           console.log(brushes, affectedBrush, affectType);
         }}
       />
@@ -93,28 +137,39 @@ const Scatterplot: FC<Props> = ({
   );
 
   const MarksLayer = (
-    <g
-      style={{ pointerEvents: mouseIsDown ? "none" : "all" }}
-      className="marks"
-    >
+    <g style={{pointerEvents: mouseIsDown ? 'none' : 'all'}} className="marks">
       {data.map((d, i) => (
         <Popup
           key={i}
           trigger={
-            <circle
-              fill={colorScale(d.color) as string}
-              cx={xScale(d.x)}
-              cy={yScale(d.y)}
-              r={5}
-            ></circle>
+            selectedIndices[i] ? (
+              selectedIndices[i] < maxIntersection ? (
+                <UnionMark
+                  fill={colorScale(d.color) as string}
+                  cx={xScale(d.x)}
+                  cy={yScale(d.y)}
+                  r={5}></UnionMark>
+              ) : (
+                <IntersectionMark
+                  fill={colorScale(d.color) as string}
+                  cx={xScale(d.x)}
+                  cy={yScale(d.y)}
+                  r={5}></IntersectionMark>
+              )
+            ) : (
+              <RegularMark
+                fill={colorScale(d.color) as string}
+                cx={xScale(d.x)}
+                cy={yScale(d.y)}
+                r={5}></RegularMark>
+            )
           }
           content={
             <div>
               <h1>{dataset.data[i].country}</h1>
               <pre>{JSON.stringify(dataset.data[i], null, 2)}</pre>
             </div>
-          }
-        ></Popup>
+          }></Popup>
       ))}
     </g>
   );
@@ -128,8 +183,7 @@ const Scatterplot: FC<Props> = ({
           <g
             ref={xAxisRef}
             transform={`translate(0, ${xScale.range()[1]})`}
-            className="x"
-          ></g>
+            className="x"></g>
           <g ref={yAxisRef} className="y"></g>
         </g>
         <g className="plot">
@@ -142,8 +196,7 @@ const Scatterplot: FC<Props> = ({
           <CloseCircle
             fill="red"
             r={10}
-            onClick={() => removePlot(plot)}
-          ></CloseCircle>
+            onClick={() => removePlot(plot)}></CloseCircle>
         )}
         )}
       </g>
@@ -152,20 +205,46 @@ const Scatterplot: FC<Props> = ({
 };
 
 const mapStateToProps = (state: VisualizationState): StateProps => ({
-  dataset: state.dataset
+  dataset: state.dataset,
 });
 
 const mapDispatchToProps = (): DispatchProps => ({
   removePlot: (plot: SinglePlot) => {
     VisualizationProvenance.apply(removePlot(plot));
-  }
+  },
+  updatePlot: (plot: SinglePlot) => {
+    VisualizationProvenance.apply(updatePlot(plot));
+  },
 });
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(Scatterplot);
 
-const CloseCircle = styled("circle")`
+const CloseCircle = styled('circle')`
   cursor: pointer;
+`;
+
+interface MarkProps {
+  fill?: string;
+}
+
+const RegularMark = styled('circle')<MarkProps>`
+  fill: ${props => (props.fill ? props.fill : 'black')};
+  opacity: 0.6;
+`;
+
+const UnionMark = styled(RegularMark)<MarkProps>`
+  fill: ${props => (props.fill ? props.fill : 'black')};
+  stroke: black;
+  stroke-width: 1.5px;
+  opacity: 0.6;
+`;
+
+const IntersectionMark = styled(RegularMark)<MarkProps>`
+  fill: ${props => (props.fill ? props.fill : 'black')};
+  stroke: red;
+  stroke-width: 1.5px;
+  opacity: 0.6;
 `;
