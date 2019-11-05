01c6ec5ed6d7024eba714bb091fe852b9f431ad8
Kiran Gadhave <kirangadhave2@gmail.com>
2019-11-05 12:39:32 -0700

    Removed debug

--- server.sh
+++ server.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
 source activate intent_server
-if [ "$#" -eq "0" ];  then
-    FLASK_APP=run.py python3 -m flask run
-else
-    FLASK_APP=run.py FLASK_DEBUG=1 python3 -m flask run
-fi
+FLASK_APP=run.py python3 -m flask run
