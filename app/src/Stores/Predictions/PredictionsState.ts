b2376e79ecd307f2c423fdec1881c9485e640b44
Kiran Gadhave <kirangadhave2@gmail.com>
2019-11-28 16:25:55 -0700

    Improved labels: Fixes #205

--- app/src/Stores/Predictions/PredictionsState.ts
+++ app/src/Stores/Predictions/PredictionsState.ts
@@ -5,3 +5,28 @@ export interface PredictionState {
   hideZeroPredictions: boolean;
   isLoading: boolean;
 }
+
+export enum PredictionType {
+  Outlier = 'Outlier',
+  Cluster = 'Cluster',
+  Skyline = 'Skyline',
+  Category = 'Category',
+  QuadraticRegression = 'QuadraticRegression',
+  LinearRegression = 'LinearRegression',
+  NonOutlier = 'Non-Outlier',
+  Range = 'Range',
+}
+
+export function getPredictionType(intent: string): PredictionType {
+  let detectedType: PredictionType = null as any;
+
+  Object.entries(PredictionType).forEach(entry => {
+    if (detectedType) return;
+    const [type, typeStr] = entry;
+    if (intent.split(':').includes(typeStr)) {
+      detectedType = type as any;
+    }
+  });
+
+  return detectedType;
+}
