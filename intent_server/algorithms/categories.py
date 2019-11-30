6f1826e4eb0f6ae763a5a9421e86c15f9ebb6ff0
Kiran Gadhave <kirangadhave2@gmail.com>
2019-11-30 13:00:17 -0700

    Fixed #215 to show category and label properly

--- intent_server/algorithms/categories.py
+++ intent_server/algorithms/categories.py
@@ -9,7 +9,7 @@ def expand_column(col: pd.DataFrame, description: str) -> pd.DataFrame:
     values = col.iloc[:, 0].unique()
     result = pd.concat(map(lambda v: pd.DataFrame(  # type: ignore
         data=(col.iloc[:, 0] == v).astype('int').values,
-        columns=[description + ":" + v],
+        columns=[description + "|" + v],
         index=col.index, dtype=int),
         values), axis='columns')
     return result
@@ -21,7 +21,7 @@ class Categories(Intent):
         self.baseName = baseName
         cats = data.categorical()
         self.expanded = pd.concat(map(lambda c: expand_column(  # type: ignore
-            cats[[c]], baseName + ":" + c + ":"), cats.columns), axis='columns')
+            cats[[c]], baseName + "|" + c + "|"), cats.columns), axis='columns')
 
     def to_string(self) -> str:
         return self.baseName
