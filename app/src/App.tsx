e1f4d1714d1f10405a5015f9561047a9c629ce41
Kiran Gadhave <kirangadhave2@gmail.com>
2019-12-02 08:23:31 -0700

    Fixed order

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -84,8 +84,8 @@ const App: FC<Props> = ({
     } else {
       const plot: SinglePlot = {
         id: new Date().valueOf().toString(),
-        x: dataset.numericColumns[0],
-        y: dataset.numericColumns[1],
+        x: dataset.numericColumns[1],
+        y: dataset.numericColumns[0],
         color: dataset.categoricalColumns[0],
         brushes: {},
         brushSelections: {},
